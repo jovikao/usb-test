f4a7126c530d11c9a375e96a6d42e84a8aa2dc9d
Jovi Kao <java.gang@gmail.com>
2018-05-14 13:46:34 +0800

    Update

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,10 @@
 console.log("Test")
+let filters = [
+    { classCode: 255, subclassCode: 66, protocolCode: 1 },
+    { classCode: 255, subclassCode: 66, protocolCode: 3 }
+];
+
+navigator.usb.requestDevice({ filters: filters })
+    .then(device => {
+        console.log(device)
+    })
